493226da0a8dc322ecce99ba5153a48da1cb4e1e
kubazaboj <87778522+kubazaboj@users.noreply.github.com>
2022-09-06 22:09:05 +0200

    Add files via upload

---
+++ funkce/funkce.inc.php
@@ -0,0 +1,5 @@
+<?php
+$migrantzosloveska_first = /*'sql112.byethost7.com'*/'localhost';
+$migrantzosloveska_second = /*'b7_21082397'*/'Hlody';
+$migrantzosloveska_last = /*'Viera9Hajkova7'*/'PjTwQ7fWD0iNHUQO';
+?>
